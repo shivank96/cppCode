e0090f116874fac2c6233083625ff4567e48d60f
shivank96 <shivankg96@gmail.com>
2023-02-07 22:31:14 +0530

    Create linearSearch.cpp
    function for searching an element linearly and if found in array returning its index position and if not found returning
    -1

---
+++ Arrays/linearSearch.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+void insertIntoArray(int a[],int n)
+{
+	cout<<"Enter the elements you want in an array"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+}
+
+void displayToDebug(int a[],int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		cout<<a[i]<<endl;
+	}
+}
+
+int linearSearch(int a[],int n,int k)
+{
+    int flag;
+	for(int i=0;i<n;i++)
+	{
+		if(a[i]==k)
+		{
+			flag = i+1;
+		} 
+	}
+	if(flag<0)
+	{
+		flag = -1;
+	}
+	return flag;
+}
+
+int main()
+{
+	int n,k;
+	cout<<"Enter the number of elements you want in an array"<<endl;
+	cin>>n;
+	int a[n];
+	//function for filling/accepting values into array
+	insertIntoArray(a,n);
+	cout<<"Enter the number that you are looking for in the array"<<endl;
+	cin>>k;
+	int flag = linearSearch(a,n,k);//function for linear search which searches for an elements index position/element linearly which you are looking for
+	if(flag >=1)
+	{
+		cout<<"the number "<<k<<" is present at index "<<flag-1;
+	}
+	else
+	{
+		cout<<-1<<endl;
+		cout<<"the number "<<k<<" is not present in the array";
+	}
+}
